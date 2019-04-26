19a24f49e7db22f1a0a924e84cd4c9b90542cdb8
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-04-26 15:02:20 +0100

    the API routs base

--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -1,15 +1,18 @@
 import express from 'express';
+import authRoutes from './auth';
 
 const apiRoutes = express.Router();
 
 apiRoutes.get('/', (req, res) => res.status(200).send({
     status: 200,
-    message: 'Welcome to the Twitter Clone API',
+    message: 'Welcome to Twitter Clone API',
 }));
 
 apiRoutes.get('/v1', (req, res) => res.status(200).send({
     status: 200,
-    message: 'Welcome to version 1 of the Twitter Clone API',
+    message: 'Welcome to version 1 of Twitter Clone API',
 }));
+
+apiRoutes.use('/v1/auth', authRoutes);
 
 export default apiRoutes;
