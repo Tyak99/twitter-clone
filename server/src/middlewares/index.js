6f273ec885add37907ee904edc1749028c04cbaa
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-04-26 14:11:16 +0100

    middlewares to power up the app

--- server/src/middlewares/index.js
+++ server/src/middlewares/index.js
@@ -1,5 +1,9 @@
-import ErrorHandler from './ErrorHandler';
+import errorHandler from './errorHandler';
+import authorization from './authorization';
+import trim from './trim';
 
 export default {
-    ErrorHandler
+    authorization,
+    errorHandler,
+    trim
 };
